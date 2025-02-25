af7ce29c898a6ea9c5779ddd24586798a8618e45
Vicky Kumar <96416858+imvicks@users.noreply.github.com>
2025-02-25 16:01:29 +0530

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
 from django.http import JsonResponse
 
 def hello_world(request):
-    return JsonResponse({"message": "Hello, World from Django API!"})
+    return JsonResponse({"message": "Hello, React from Django API!"})
